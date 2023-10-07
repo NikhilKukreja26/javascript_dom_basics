46975991c92fda4c05edfce8899059f5c702b452
NikhilKukreja26 <kukrejanikhil25@gmail.com>
2023-10-07 14:12:32 +0530

    first-commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,35 @@
+// const message = document.getElementById('message');
+// console.log(message);
+// const inputs = document.getElementsByName('language');
+// console.log(inputs);
+// const headings = document.getElementsByTagName('h1');
+// console.log(headings);
+// const headings = document.getElementsByClassName('message');
+// console.log(headings);
+// const container = document.getElementById('container');
+// const headings = container.getElementsByClassName('message');
+// console.log(headings);
+
+// const message = document.querySelector('.message');
+// console.log(message);
+// const message = document.querySelectorAll('.message');
+// console.log(message);
+// const container = document.getElementById('container');
+// const message = container.querySelectorAll('.message');
+// const message = document.querySelectorAll('h1');
+// const message = document.querySelectorAll('div, h1');
+// console.log(message);
+
+//?  Traversing Elements
+// const text = document.querySelector('.text');
+// console.log(text.parentNode);
+
+//? Getting ChildNodes,Children,FirstChild, LastChild, FirstElementChild, LastElementChild
+// const welcome = document.querySelector('.welcome');
+// console.log(welcome.firstChild);
+// console.log(welcome.firstElementChild);
+// console.log(welcome.lastChild);
+// console.log(welcome.lastElementChild);
+// console.log(welcome.childNodes);
+// console.log(welcome.childElementCount);
+// console.log(welcome.children);
